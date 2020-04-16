689818d8b4b0f9d513772c0c6cda98b1d76fbb00
Lothar Ulz <63649401+LotharUlz@users.noreply.github.com>
2020-04-16 10:39:47 +0200

    Update PolicyRepository.java

--- graphqldemo/src/main/java/com/nttdata/graphqldemo/PolicyRepository.java
+++ graphqldemo/src/main/java/com/nttdata/graphqldemo/PolicyRepository.java
@@ -1,8 +1,14 @@
 package com.nttdata.graphqldemo;
 
+import javax.annotation.sql.DataSourceDefinition;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@DataSourceDefinition(
+        name="java:app/MyDatasource",
+        className="org.h2.jdbcx.JdbcDataSource",
+        url="jdbc:h2:mem:test")
 interface PolicyRepository extends JpaRepository<Policy, Long> {
 }
