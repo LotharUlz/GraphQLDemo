3c503738d0838c9e0b0730a1fba375e09e41ee1e
Lothar Ulz <63649401+LotharUlz@users.noreply.github.com>
2020-04-29 13:56:23 +0200

    Update users.sql

--- etc/users.sql
+++ etc/users.sql
@@ -1,18 +1,18 @@
 CREATE SCHEMA partner;
 
 CREATE TABLE partner.users (
-  id bigint NOT NULL,
+  id serial NOT NULL,
   name varchar(255) NOT NULL,
   countryCodeId varchar(3) NULL,
   constraint pk_users primary key (id)
 );
 
-INSERT INTO partner.users (id, name, countryCodeId)
+INSERT INTO partner.users (name, countryCodeId)
 VALUES
-    (1, 'Marge Simpson', 'US'),
-    (2, 'Homer Simpson', 'US'),
-    (3, 'Bart Simpson', 'US'),
-    (4, 'Lisa Simpson', 'US'),
-    (5, 'Maggie Simpson', 'US'),
-    (6, 'Itchy', 'US'),
-    (7, 'Scratchy', 'US');
+    ('Marge Simpson', 'US'),
+    ('Homer Simpson', 'US'),
+    ('Bart Simpson', 'US'),
+    ('Lisa Simpson', 'US'),
+    ('Maggie Simpson', 'US'),
+    ('Itchy', 'US'),
+    ('Scratchy', 'US');
