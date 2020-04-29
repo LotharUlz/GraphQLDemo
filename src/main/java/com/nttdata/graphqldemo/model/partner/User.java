e9ac063e8556921d7129335459a6ed0a172cea9a
Lothar Ulz <63649401+LotharUlz@users.noreply.github.com>
2020-04-29 12:23:54 +0200

    Update User.java

--- src/main/java/com/nttdata/graphqldemo/model/partner/User.java
+++ src/main/java/com/nttdata/graphqldemo/model/partner/User.java
@@ -7,13 +7,17 @@ import io.leangen.graphql.annotations.GraphQLQuery;
 @Table(name = "users", schema = "partner")
 public class User {
 
-    @Id @GeneratedValue
+    @Id 
+    @GeneratedValue
+    @Column(name="id")
     @GraphQLQuery(name = "id", description = "A user's id")
     private Long id;
 
     @GraphQLQuery(name = "name", description = "A user's name")
+    @Column(name="name")
     private String name;
     
+    @Column(name="countryCodeId")
     private String countryCodeId;
     
     public Long getId() {
