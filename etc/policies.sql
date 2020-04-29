26b2f25eb56a4e45de1ddf49f4128562a8ee5cad
Lothar Ulz <63649401+LotharUlz@users.noreply.github.com>
2020-04-29 13:57:48 +0200

    Update policies.sql

--- etc/policies.sql
+++ etc/policies.sql
@@ -1,14 +1,15 @@
 CREATE SCHEMA policy;
 
 CREATE TABLE policy.policies (
-  id bigint NOT NULL,
+  id serial NOT NULL,
   name varchar(255) NOT NULL,
   userId bigint NULL,
   constraint pk_policies primary key (id)
 );
 
-INSERT INTO policy.policies (id, name, userId)
+INSERT INTO policy.policies (name, userId)
 VALUES
-    (1, 'Haftpflicht', 3),
-    (2, 'Naturkatastrophen', 2),
-    (3, 'KFZ', 1);
+    ('Haftpflicht', 3),
+    ('Hausrat', 3),
+    ('Naturkatastrophen', 2),
+    ('KFZ', 1);
